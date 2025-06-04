d0451649c4f7edfed6ac7a00c3beec656e707359
Akram Nsir <akramnsir5@gmail.com>
2025-06-04 12:47:11 +0100

    Fixed a bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-import { getData } from "@/actions/todoAction";
-import { getAllUsers, getUser } from "@/actions/userAction";
-import Todos from "@/components/todos";
+// import { getData } from "@/actions/todoAction";
+// import { getAllUsers, getUser } from "@/actions/userAction";
+// import Todos from "@/components/todos";
 
 export default async function Home() {
-  const users = await getAllUsers();
-  console.log(users)
-  const data = await getData(users[0].id);
+  // const users = await getAllUsers();
+  // console.log(users)
+  // const data = await getData(users[0].id);
 
-  const user = await getUser(users[0].id)
-  console.log(user);
+  // const user = await getUser(users[0].id)
+  // console.log(user);
   return (
     <main>
-      <Todos todos={data} user={users[0]} />
+      {/* <Todos todos={data} user={users[0]} /> */}
     </main>
   );
 }
